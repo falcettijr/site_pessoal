0381cb467498f7696e2cebf028a26ebbbd332922
sergio falcetti <sergio.falcetti@codenapp.com>
2023-09-01 10:57:31 -0300

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,15 @@
 import SocialNetworkContainer from "./SocialNetworkContainer";
 import InformationContainer from "./InformationContainer";
 
-import Avatar from "../img/eu.jpg";
+import Avatar from "../img/sergiofalcetti.png";
 
 import "../styles/components/sidebar.sass";
-
+  
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Sergio Falcetti" />
-      <p className="title">Desenvolvedor</p>
+      <p className="title">Desenvolvedor Front-End</p>
       <SocialNetworkContainer />
       <InformationContainer />
       <a href="#" className="btn">
